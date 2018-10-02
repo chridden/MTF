3fa39d97d77fd1541bafed6449866be795908c41
Jack Pickering <jackpickering@admins-air.home>
2018-10-02 15:38:26 +0100

    Added all contracts to split_token

---
+++ contracts/split_token/MetaFusion.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+
+import './Ownable.sol';
+import './ERC20_allowance.sol';
+
+contract MetaFusion is Ownable, ERC20_allowance {
+	string public name;
+	string public symbol;
+	uint8 public decimals;
+
+	constructor(string _name, string _symbol, uint _decimals) public{
+		name = _name;
+		symbol = _symbol;
+		decimals = _decimals;
+	}
+}
